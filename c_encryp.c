29a18ab6f0d53281d9485d104abf4e98b171dfdc
methy-dino <gutotlima@gmail.com>
2025-03-09 13:23:45 -0300

    added security to batch encryption and granted the inclusion of the null terminator.

--- c_encryp.c
+++ c_encryp.c
@@ -4,19 +4,27 @@
 #include<stdlib.h>
 // can't encrypt in place due to chance of bad size
 char* batch_encrypt(char* str, size_t len, size_t* n_len, char* key, size_t key_l){
-	//ceil(len / 16) * 16; 
-	n_len[0] = ((len + 15) / 16) * 16;
+    // +1 to grant space for null terminator
+    //ceil((len+1) / 16) * 16;
+	n_len[0] = ((len + 16) / 16) * 16;
+    unsigned int seed = 0;
+	for (size_t i = 0; i < key_l; i++){
+		seed += key[i] * 11;
+	}
+    srand(seed);
 	char* encrypted = malloc(n_len[0]);
 	size_t i = 0;
 	while(i < len){
 		encrypted[i] = str[i];
 		i++;
 	}
-	// better than calling calloc
+    encrypted[len] = '\0';
+    i++;
+	// garbage data for safety
 	while(i < n_len[0]){
-		encrypted[i] = '\0';
+		encrypted[i] = rand() % 256;
 		i++;
-	}
+	} 
 	for (size_t i = 0; i < n_len[0] / 16; i++){
 		// sets up a pointer to the current batch and sends it to encryption.
 		encrypt_data(&encrypted[i*16], key, key_l);
@@ -26,7 +34,7 @@ char* batch_encrypt(char* str, size_t len, size_t* n_len, char* key, size_t key_
 // should always be a multiple of 16, it is a pair to batch_encrypt.
 void batch_decrypt(char* encrypted, size_t len, char* key, size_t key_l){
 	for (size_t i = 0; i < len / 16; i++){
-		decrypt_data(&encrypted[i*16], key, key_l);
+        decrypt_data(&encrypted[i*16], key, key_l);
 	}
 	// shrimple
 }
